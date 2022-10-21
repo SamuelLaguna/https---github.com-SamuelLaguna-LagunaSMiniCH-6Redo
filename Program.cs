794c499eb6c4c463b0a8db5872158cbd237f30ef
SamuelLaguna <samanater11@gmail.com>
2022-10-21 10:00:45 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Samuel Laguna
+//10 - 20 - 22
+//We are to redo challenge6 with data validation and a play again feture.
+
+
+Console.Clear();
+int i;
+bool playAgain = true;
+bool isConverted;
+bool end = false;
+while (playAgain == true)
+{
+Console.WriteLine("enter your number here");
+     isConverted = Int32.TryParse(Console.ReadLine(), out i);
+    Console.WriteLine(i % 2 == 0 ? "even" : "odd");
+
+   
+
+        Console.WriteLine("Enter end to exit, or type play again to restart ");
+
+    // string name = Console.ReadLine();
+    string ending = Console.ReadLine();
+      
+        if (ending == "end")
+        {
+            playAgain = false;
+           end = true;
+        }
+ 
+
+
+
+}
+
